5a884ea493f3f483411f3fed60c7c51c847e79ed
jdeokkim <jdeokkim@protonmail.com>
2021-08-08 14:14:04 +0000

    Implement `BinaryHeap` in `binary_heap.h`

---
+++ include/binary_heap.h
@@ -0,0 +1,150 @@
+/*
+    Copyright (c) 2021 jdeokkim
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#ifndef BINARY_HEAP_H
+#define BINARY_HEAP_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BINARY_HEAP_DEF static
+
+/* 이진 힙을 나타내는 구조체. */
+typedef struct BinaryHeap {
+    int length;
+    int capacity;
+    unsigned int *ptr;
+} BinaryHeap;
+
+/* 이진 힙을 생성한다. */
+BINARY_HEAP_DEF BinaryHeap *binary_heap_create(void);
+
+/* 이진 힙 `heap`에 할당된 메모리를 해제한다. */
+BINARY_HEAP_DEF void binary_heap_release(BinaryHeap *heap);
+
+/* 이진 힙 `heap`에 들어 있는 모든 값을 제거한다. */
+BINARY_HEAP_DEF void binary_heap_clear(BinaryHeap *heap);
+
+/* 이진 힙 `heap`에 들어 있는 값의 개수를 반환한다. */
+BINARY_HEAP_DEF int binary_heap_size(BinaryHeap *heap);
+
+/* 이진 힙 `heap`이 비어 있는지 확인한다. */
+BINARY_HEAP_DEF bool binary_heap_is_empty(BinaryHeap *heap);
+
+/* 이진 힙 `heap`에 값 `value`를 추가한다. */
+BINARY_HEAP_DEF void binary_heap_push(BinaryHeap *heap, unsigned int value);
+
+/* 이진 힙 `heap`에서 최댓값을 삭제하고, 그 값을 반환한다. */
+BINARY_HEAP_DEF int binary_heap_pop(BinaryHeap *heap);
+
+#endif // `BINARY_HEAP_H`
+
+#ifdef BINARY_HEAP_IMPLEMENTATION
+
+/* 이진 힙을 생성한다. */
+BINARY_HEAP_DEF BinaryHeap *binary_heap_create(void) {
+    BinaryHeap *result = calloc(1, sizeof(BinaryHeap));
+    
+    result->capacity = 8;
+    result->ptr = calloc(result->capacity, sizeof(unsigned int));
+    
+    return result;
+}
+
+/* 이진 힙 `heap`에 할당된 메모리를 해제한다. */
+BINARY_HEAP_DEF void binary_heap_release(BinaryHeap *heap) {
+    free(heap->ptr);
+    free(heap);
+}
+
+/* 이진 힙 `heap`에 들어 있는 모든 값을 제거한다. */
+BINARY_HEAP_DEF void binary_heap_clear(BinaryHeap *heap) {
+    if (heap != NULL) heap->length = 0;
+}
+
+/* 이진 힙 `heap`에 들어 있는 값의 개수를 반환한다. */
+BINARY_HEAP_DEF int binary_heap_size(BinaryHeap *heap) {
+    return (heap != NULL) ? heap->length : 0;
+}
+
+/* 이진 힙 `heap`이 비어 있는지 확인한다. */
+BINARY_HEAP_DEF bool binary_heap_is_empty(BinaryHeap *heap) {
+    return (heap == NULL) || (heap != NULL && heap->length == 0);
+}
+
+/* 이진 힙 `heap`에 값 `value`를 추가한다. */
+BINARY_HEAP_DEF void binary_heap_push(BinaryHeap *heap, unsigned int value) {
+    if (heap == NULL) return;
+    
+    if (heap->length >= heap->capacity) {
+        heap->capacity *= 2;
+        heap->ptr = realloc(heap->ptr, 2 * heap->capacity * sizeof(unsigned int));
+    }
+    
+    heap->ptr[++heap->length] = value;
+    
+    int i = heap->length;
+    
+    // 이진 힙을 상향식으로 복구한다.
+    while (i > 1 && heap->ptr[i] > heap->ptr[i / 2]) {
+        int temp_value = heap->ptr[i];
+        
+        heap->ptr[i] = heap->ptr[i / 2];
+        heap->ptr[i / 2] = temp_value;
+        
+        i /= 2;
+    }
+}
+
+/* 이진 힙 `heap`에서 최댓값을 삭제하고, 그 값을 반환한다. */
+BINARY_HEAP_DEF int binary_heap_pop(BinaryHeap *heap) {
+    if (heap == NULL || heap->length == 0) return -1;
+    
+    int result = heap->ptr[1];
+    
+    heap->ptr[1] = heap->ptr[heap->length];
+    heap->ptr[heap->length--] = -1;
+    
+    int i = 1;
+    
+    // 이진 힙을 하향식으로 복구한다.
+    while (2 * i <= heap->length) {
+        int j = 2 * i;
+        
+        if (heap->ptr[j] < heap->ptr[j + 1] && j + 1 <= heap->length) j++;
+        
+        // 부모 노드가 자식 노드보다 크면 복구를 마친다.
+        if (heap->ptr[i] > heap->ptr[j]) break;
+        
+        int temp_value = heap->ptr[i];
+        
+        heap->ptr[i] = heap->ptr[j];
+        heap->ptr[j] = temp_value;
+        
+        i = j;
+    }
+    
+    return result;
+}
+
+#endif // `BINARY_HEAP_IMPLEMENTATION`
--- include/linked_list.h
+++ include/linked_list.h
@@ -28,135 +28,169 @@
 
 #define LINKED_LIST_DEF static
 
+/* 단일 연결 리스트의 항목을 나타내는 구조체. */
+typedef struct SinglyLinkedNode {
+    int value;
+    struct SinglyLinkedNode *next;
+} SinglyLinkedNode;
+
 /* 단일 연결 리스트를 나타내는 구조체. */
 typedef struct SinglyLinkedList {
-    int value;
-    struct SinglyLinkedList *next;
+    SinglyLinkedNode *front;
+    SinglyLinkedNode *back;
 } SinglyLinkedList;
 
 /* 단일 연결 리스트를 생성한다. */
-LINKED_LIST_DEF SinglyLinkedList *sll_create(int value);
+LINKED_LIST_DEF SinglyLinkedList *sll_create(void);
 
 /* 단일 연결 리스트 `ll`에 할당된 메모리를 해제한다. */
 LINKED_LIST_DEF void sll_release(SinglyLinkedList *ll);
 
-/* 단일 연결 리스트 `ll`와 연결된 모든 값을 제거한다. */
+/* 단일 연결 리스트 `ll`의 모든 항목을 제거한다. */
 LINKED_LIST_DEF void sll_clear(SinglyLinkedList *ll);
 
 /* 단일 연결 리스트 `ll`의 시작 부분을 반환한다. */
-LINKED_LIST_DEF SinglyLinkedList *sll_front(SinglyLinkedList *ll);
+LINKED_LIST_DEF SinglyLinkedNode *sll_front(SinglyLinkedList *ll);
 
 /* 단일 연결 리스트 `ll`의 끝 부분을 반환한다. */
-LINKED_LIST_DEF SinglyLinkedList *sll_back(SinglyLinkedList *ll);
+LINKED_LIST_DEF SinglyLinkedNode *sll_back(SinglyLinkedList *ll);
 
-/* 단일 연결 리스트 `ll`의 시작 부분에 값 `value`를 추가한다. */
-LINKED_LIST_DEF void sll_push_front(SinglyLinkedList **ll, int value);
+/* 단일 연결 리스트 `ll`의 시작 부분에 새로운 항목을 추가한다. */
+LINKED_LIST_DEF void sll_push_front(SinglyLinkedList *ll, int value);
 
-/* 단일 연결 리스트 `ll`의 끝 부분에 값 `value`를 추가한다. */
-LINKED_LIST_DEF void sll_push_back(SinglyLinkedList **ll, int value);
+/* 단일 연결 리스트 `ll`의 끝 부분에 새로운 항목을 추가한다. */
+LINKED_LIST_DEF void sll_push_back(SinglyLinkedList *ll, int value);
 
-/* 단일 연결 리스트 `ll`의 시작 부분의 값을 제거하고, 그 값을 반환한다. */
-LINKED_LIST_DEF int sll_pop_front(SinglyLinkedList **ll);
+/* 단일 연결 리스트 `ll`의 시작 부분의 항목을 제거하고, 그 항목의 값을 반환한다. */
+LINKED_LIST_DEF int sll_pop_front(SinglyLinkedList *ll);
 
-/* 단일 연결 리스트 `ll`의 끝 부분의 값을 제거하고, 그 값을 반환한다. */
-LINKED_LIST_DEF int sll_pop_back(SinglyLinkedList **ll);
+/* 단일 연결 리스트 `ll`의 끝 부분의 항목을 제거하고, 그 항목의 값을 반환한다. */
+LINKED_LIST_DEF int sll_pop_back(SinglyLinkedList *ll);
 
 #endif // `LINKED_LIST_H`
 
 #ifdef LINKED_LIST_IMPLEMENTATION
 
 /* 단일 연결 리스트를 생성한다. */
-LINKED_LIST_DEF SinglyLinkedList *sll_create(int value) {
-    SinglyLinkedList *result = calloc(1, sizeof(SinglyLinkedList));
+LINKED_LIST_DEF SinglyLinkedList *sll_create(void) {
+    return calloc(1, sizeof(SinglyLinkedList));
+}
+
+/* 단일 연결 리스트 `ll`에 할당된 메모리를 해제한다. */
+LINKED_LIST_DEF void sll_release(SinglyLinkedList *ll) {
+    sll_clear(ll);
+    
+    free(ll);
+}
+
+/* 단일 연결 리스트의 항목 `node`에 할당된 메모리를 해제한다. */
+LINKED_LIST_DEF void _sll_node_release(SinglyLinkedNode *node) {
+    free(node);
+}
+
+/* 단일 연결 리스트의 항목 `node`와 연결된 모든 항목을 제거한다. */
+LINKED_LIST_DEF void _sll_node_clear(SinglyLinkedNode *node) {
+    if (node == NULL || node->next == NULL) return;
+    
+    _sll_node_clear(node->next);
+    _sll_node_release(node->next);
+    
+    node->next = NULL;
+}
+
+/* 단일 연결 리스트 `ll`의 모든 항목을 제거한다. */
+LINKED_LIST_DEF void sll_clear(SinglyLinkedList *ll) {
+    if (ll == NULL) return;
+    
+    _sll_node_clear(ll->front);
+    _sll_node_release(ll->front);
+    
+    ll->front = ll->back = NULL;
+}
+
+/* 단일 연결 리스트 `ll`의 시작 부분을 반환한다. */
+LINKED_LIST_DEF SinglyLinkedNode *sll_front(SinglyLinkedList *ll) {
+    return ll->front;
+}
+
+/* 단일 연결 리스트 `ll`의 끝 부분을 반환한다. */
+LINKED_LIST_DEF SinglyLinkedNode *sll_back(SinglyLinkedList *ll) {
+    return ll->back;
+}
+
+/* 단일 연결 리스트를 생성한다. */
+LINKED_LIST_DEF SinglyLinkedNode *_sll_node_create(int value) {
+    SinglyLinkedNode *result = calloc(1, sizeof(SinglyLinkedNode));
     
     result->value = value;
     
     return result;
 }
 
-/* 단일 연결 리스트 `ll`에 할당된 메모리를 해제한다. */
-LINKED_LIST_DEF void sll_release(SinglyLinkedList *ll) {
-    sll_clear(ll);
-    free(ll);
-}
-
-/* 단일 연결 리스트 `ll`와 연결된 모든 값을 제거한다. */
-LINKED_LIST_DEF void sll_clear(SinglyLinkedList *ll) {
-    if (ll != NULL && ll->next != NULL) {
-        sll_release(ll->next);
-        ll->next = NULL;
+/* 단일 연결 리스트 `ll`의 시작 부분에 새로운 항목을 추가한다. */
+LINKED_LIST_DEF void sll_push_front(SinglyLinkedList *ll, int value) {
+    if (ll->front == NULL && ll->back == NULL) {
+        ll->front = ll->back = _sll_node_create(value);
+    } else {
+        SinglyLinkedNode *front = _sll_node_create(value);
+        
+        front->next = ll->front;
+        
+        ll->front = front;
     }
 }
 
-/* 단일 연결 리스트 `ll`의 시작 부분을 반환한다. */
-LINKED_LIST_DEF SinglyLinkedList *sll_front(SinglyLinkedList *ll) {
-    return ll;
-}
-
-/* 단일 연결 리스트 `ll`의 끝 부분을 반환한다. */
-LINKED_LIST_DEF SinglyLinkedList *sll_back(SinglyLinkedList *ll) {
-    SinglyLinkedList *ptr = ll;
-    
-    while (ptr->next != NULL) 
-        ptr = ptr->next;
-    
-    return ptr;
-}
-
-/* 단일 연결 리스트 `ll`의 시작 부분에 값 `value`를 추가한다. */
-LINKED_LIST_DEF void sll_push_front(SinglyLinkedList **ll, int value) {
-    if (ll == NULL || *ll == NULL) return;
-    
-    SinglyLinkedList *front = sll_create(value);
-    front->next = *ll;
-    
-    *ll = front;
-}
-
-/* 단일 연결 리스트 `ll`의 끝 부분에 값 `value`를 추가한다. */
-LINKED_LIST_DEF void sll_push_back(SinglyLinkedList **ll, int value) {
-    if (ll == NULL || *ll == NULL) return;
-    
-    SinglyLinkedList *back = sll_back(*ll);
-    
-    back->next = sll_create(value);
-}
-
-/* 단일 연결 리스트 `ll`의 시작 부분의 값을 제거하고, 그 값을 반환한다. */
-LINKED_LIST_DEF int sll_pop_front(SinglyLinkedList **ll) {
-    if (ll == NULL || *ll == NULL) return -1;
-    
-    SinglyLinkedList *ptr = (*ll)->next;
-    
-    int result = (*ll)->value;
-    
-    (*ll)->next = NULL;
-    sll_release(*ll);
-    
-    *ll = ptr;
-}
-
-/* 단일 연결 리스트 `ll`의 끝 부분의 값을 제거하고, 그 값을 반환한다. */
-LINKED_LIST_DEF int sll_pop_back(SinglyLinkedList **ll) {
-    if (ll == NULL || *ll == NULL) return -1;
-    
-    SinglyLinkedList *ptr = *ll;
-    
-    if (ptr->next == NULL) {
-        int result = ptr->value;
+/* 단일 연결 리스트 `ll`의 끝 부분에 새로운 항목을 추가한다. */
+LINKED_LIST_DEF void sll_push_back(SinglyLinkedList *ll, int value) {
+    if (ll->front == NULL && ll->back == NULL) {
+        sll_push_front(ll, value);
+    } else {
+        SinglyLinkedNode *back = _sll_node_create(value);
         
-        sll_release(*ll);
-        *ll = NULL;
+        ll->back->next = back;
+        ll->back = back;
+    }
+}
+
+/* 단일 연결 리스트 `ll`의 시작 부분의 항목을 제거하고, 그 항목의 값을 반환한다. */
+LINKED_LIST_DEF int sll_pop_front(SinglyLinkedList *ll) {
+    if (ll == NULL || ll->front == NULL) return -1;
+    
+    SinglyLinkedNode *front = ll->front;
+    
+    int result = front->value;
+    
+    _sll_node_release(ll->front);
+    
+    if (front->next == NULL) ll->front = ll->back = NULL;
+    else ll->front = front->next;
+    
+    return result;
+}
+
+/* 단일 연결 리스트 `ll`의 끝 부분의 항목을 제거하고, 그 항목의 값을 반환한다. */
+LINKED_LIST_DEF int sll_pop_back(SinglyLinkedList *ll) {
+    if (ll == NULL || ll->back == NULL) return -1;
+    
+    SinglyLinkedNode *front = ll->front;
+    
+    if (front->next == NULL) {
+        int result = front->value;
+        
+        _sll_node_release(front);
+        
+        ll->front = ll->back = NULL;
         
         return result;
     } else {
-        while (ptr->next != NULL && ptr->next->next != NULL)
-            ptr = ptr->next;
+        while (front->next != NULL && front->next->next != NULL)
+            front = front->next;
         
-        int result = ptr->next->value;
+        int result = front->next->value;
         
-        sll_release(ptr->next);
-        ptr->next = NULL;
+        _sll_node_release(front->next);
+        
+        front->next = NULL;
         
         return result;
     }
